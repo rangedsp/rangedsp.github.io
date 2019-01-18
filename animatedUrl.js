3567f2376a308e16ee6138d1069c018656ac4426
Jonny Lin <jonny152007@hotmail.com>
2019-01-18 01:51:51 -0800

    Add files via upload

---
+++ animatedUrl.js
@@ -0,0 +1,40 @@
+var f = ['🌑', '🌘', '🌗', '🌖', '🌕', '🌔', '🌓', '🌒'],
+        d = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        m = 0;
+
+    function loop() {
+        var s = '', x = 0;
+
+        if (!m) {
+            while (d[x] == 4) {
+                x ++;
+            }
+
+            if (x >= d.length) m = 1;
+            else {
+                d[x] ++;
+            }
+        }
+        else {
+            while (d[x] == 0) {
+                x ++;
+            }
+
+            if (x >= d.length) m = 0;
+            else {
+                d[x] ++;
+
+                if (d[x] == 8) d[x] = 0;
+            }
+        }
+
+        d.forEach(function (n) {
+            s += f[n];
+        });
+
+        location.hash = s;
+
+        setTimeout(loop, 50);
+    }
+
+    loop();
